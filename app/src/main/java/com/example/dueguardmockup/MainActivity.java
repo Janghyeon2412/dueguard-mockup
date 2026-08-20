16d1d6f6d33079d56987161e115c1be53c71746f
gahyeon <janghyeon2412@gmail.com>
2026-08-20 10:49:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dueguardmockup/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.dueguardmockup;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            android.app.NotificationChannel channel = new android.app.NotificationChannel(
+                    "due_guard_channel", // 채널 ID
+                    "중요 알림",
+                    android.app.NotificationManager.IMPORTANCE_HIGH // 팝업 띄우는 핵심 설정
+            );
+            android.app.NotificationManager manager = getSystemService(android.app.NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(channel);
+            }
+        }
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(task -> {
+                    if (!task.isSuccessful()) {
+                        Log.w("FCM", "토큰 받기 실패", task.getException());
+                        return;
+                    }
+                    String token = task.getResult();
+                    Log.d("FCM", "토큰: " + token);
+                });
+    }
+}
